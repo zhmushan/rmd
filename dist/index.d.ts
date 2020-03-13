4c8c0cc1e980b58870d651918c288abab9f69701
zhmushan <zhmushan@qq.com>
2020-03-13 13:41:19 +0800

    update type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,10 @@
 import React from "react";
-export declare function Markdown({ src, children }: {
-    src?: string;
-} & React.PropsWithChildren<{}>): JSX.Element;
+declare type PropsWithSrc = {
+    src: string;
+    children?: undefined;
+};
+declare type PropsWithChildren = {
+    src?: undefined;
+} & React.PropsWithChildren<{}>;
+export declare function Markdown({ src, children }: PropsWithSrc | PropsWithChildren): JSX.Element;
+export {};
--- index.tsx
+++ index.tsx
@@ -3,8 +3,16 @@ import React, { useState, useEffect } from "react";
 
 const md = new MarkdownIt();
 
+type PropsWithSrc = {
+  src: string;
+  children?: undefined;
+};
+type PropsWithChildren = {
+  src?: undefined;
+} & React.PropsWithChildren<{}>;
+
 export function Markdown(
-  { src, children }: { src?: string } & React.PropsWithChildren<{}>
+  { src, children }: PropsWithSrc | PropsWithChildren
 ) {
   let v = "", setv: React.Dispatch<React.SetStateAction<string>>;
   if (src) {
